d2c343acac3f8d653792059bbdf3efaf4d7b81c1
Olivier2712 <olegras@udesa.edu.ar>
2022-11-18 09:28:13 -0300

    Cambio

--- src/screens/Post/CreatePost.jsx
+++ src/screens/Post/CreatePost.jsx
@@ -1,29 +1,39 @@
 
 import { useState } from "react";
 import { Button, StyleSheet, Text, TouchableOpacity, View, TextInput } from "react-native";
+import MyCamera from "../../components/MyCamera";
 import authenticationConsumer from "../../firebase/authentication/authenticationProvider";
 import { FIRESTORE_FOLDER } from "../../firebase/dataBase/fireStoreFolders";
 import { store } from "../../firebase/dataBase/fireStoreService";
 
 
-export default function CreatePost() {
-const {user} = authenticationConsumer()
-const [descripcion, onChangeDescrpcion] = useState("")
-function savePost(){
-    const {uid, email} = user
+export default class CreatePost extends React.Component {
+  const { user } = authenticationConsumer()
+  const [descripcion, onChangeDescrpcion] = useState("")
+  function savePost() {
+    const { uid, email } = user
     const newPost = {
-        descripcion, createAt: (new Date().toISOString()), email
+      descripcion, createAt: (new Date().toISOString()), email
     }
-    store(uid, FIRESTORE_FOLDER.POST, newPost ) 
-        .then(response => {console.log(response); navigation.navigate("Home")})
-}
+    store(uid, FIRESTORE_FOLDER.POST, newPost)
+      .then(response => { console.log(response); navigation.navigate("Home") })
+
+  }
+const 
+  onImageUpload(url){
+    this.setState({
+      url,
+      showCamera: false
+    })
+  }
 
   return (
     <View>
-        <Text> Crea tu post </Text>
-        <Text> Descripcion </Text>
-        <TextInput value={descripcion} onChangeText={onChangeDescrpcion} keyBoardType="default" />
-        <Button title="Guardar" onPress={savePost}/>
+      <Text> Crea tu post </Text>
+      <MyCamera />
+      <Text> Descripcion </Text>
+      <TextInput value={descripcion} onChangeText={onChangeDescrpcion} keyBoardType="default" />
+      <Button title="Guardar" onPress={savePost} />
     </View>
   );
 }
